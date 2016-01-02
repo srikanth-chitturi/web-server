ad0d39b9abc70de3bd194439716ef8a3f0f112b0
Srikanth Chitturi <ysrikanth2011@gmail.com>
2016-01-02 15:48:23 +0530

    initialize repo

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+var middleware = {
+	requireAuthentication:function(req,res,next){
+		console.log('private route hit');
+		next();
+	},
+	logger:function(req,res,next){
+		console.log('req.method :'+req.method+" URL :"+req.originalUrl+" date :"+new Date().toString());
+		next();
+	}
+}
+
+//app.use(middleware.requireAuthentication);
+app.use(middleware.logger);
+
+app.get('/about',function(req,res) {
+	res.send('About US');
+});
+
+app.use(express.static(__dirname+'/public'));
+
+app.listen(port,function(){
+	console.log('express server started :'+port);
+});
