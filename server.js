97305fe4aa5fac38cf0ed0186e5837c87ba2ddf1
Srikanth Chitturi <ysrikanth2011@gmail.com>
2016-01-22 14:38:37 +0530

    Modified the server

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ var port = process.env.PORT || 3000;
 var middleware = require('./middleware.js')
 
 //app.use(middleware.requireAuthentication);
+
 app.use(middleware.logger);
+
 
 app.get('/about',function(req,res) {
 	res.send('About US');
@@ -17,8 +19,11 @@ app.get('/login',middleware.requireAuthentication,function(req,res) {
 	res.send('Login here');
 });
 
+
 app.use(express.static(__dirname+'/public'));
+
 
 app.listen(port,function(){
 	console.log('express server started :'+port);
 });
+
